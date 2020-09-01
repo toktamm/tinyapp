0a82504378f017d95a2def3d189206a065e284d7
Toktam <ttmohebbi@gmail.com>
2020-09-01 04:24:53 +0000

    added urls template

--- express_server.js
+++ express_server.js
@@ -25,3 +25,9 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+
