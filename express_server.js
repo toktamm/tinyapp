399613bf459db9de8883cb68c544fa14a6de6da9
Toktam <ttmohebbi@gmail.com>
2020-09-01 04:49:45 +0000

    added second route and template

--- express_server.js
+++ express_server.js
@@ -30,4 +30,9 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
+
 
