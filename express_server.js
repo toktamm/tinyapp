e17978aa1cbb8f9aa5889350869581e402e0eaee
Toktam <ttmohebbi@gmail.com>
2020-09-29 21:45:23 +0000

    removed unnecessary routes

--- express_server.js
+++ express_server.js
@@ -41,14 +41,6 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls", (req, res) => {
   const user_id = req.session.user_id;
   if (!user_id) {
